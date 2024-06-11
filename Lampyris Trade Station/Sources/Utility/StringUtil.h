661b561d6f091f2bfe0e6e4023300cf7307b011d
wushuhong <390031942@qq.com>
2024-06-11 22:12:16 +0800

    2024/6/11

--- Lampyris Trade Station/Sources/Utility/StringUtil.h
+++ Lampyris Trade Station/Sources/Utility/StringUtil.h
@@ -10,7 +10,7 @@
 
 namespace StringUtil {
     /*
-     * 正则表达式匹配IPv4地址
+     * 判断 输入字符串是否符合IPv4地址格式
     */
     bool isValidIPV4(const QString& ip) {
         QRegularExpression regex("^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$");
@@ -19,4 +19,20 @@ namespace StringUtil {
         // 如果匹配成功，则返回true，表示是有效的IP地址
         return match.hasMatch();
     }
+
+    /*
+     * 判断 输入字符串是否符合网络端口号格式
+    */
+    bool isValidPort(const QString & portStr) {
+        QRegularExpression regex("^([1-9][0-9]{0,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$");
+        QRegularExpressionMatch match = regex.match(portStr);
+
+        // 如果匹配成功，并且转换后的数字在1到65535之间，则返回true
+        if (match.hasMatch()) {
+            int port = portStr.toInt();
+            return port >= 1 && port <= 65535;
+        }
+        return false;
+    }
+
 } // end of namespace 'StringUtil'
