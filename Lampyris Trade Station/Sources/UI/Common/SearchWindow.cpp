5024fa6da4a338af8dfcadf971af585613486daa
wsh39 <wsh39@DESKTOP-39V3VNQ>
2024-07-01 00:43:07 +0800

    2024/7/1

---
+++ Lampyris Trade Station/Sources/UI/Common/SearchWindow.cpp
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) HongJin Investment Ltd. All rights reserved.
+*/
+
+// Project Include(s)
+#include "SearchWindow.h"
+
+SearchWindow::SearchWindow() {}
---
+++ Lampyris Trade Station/Sources/UI/Common/SearchWindow.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) HongJin Investment Ltd. All rights reserved.
+*/
+#pragma once
+
+// QT Include(s)
+#include <QHBoxLayout>
+#include <QWidget>
+#include <QPainter>
+#include <QMouseEvent>
+
+class SearchWindow :public QWidget {
+	Q_OBJECT
+public:
+	SearchWindow();
+};
