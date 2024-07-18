a8c776981f4386314e02c2d6bc2bf854c2b577be
wsh39 <wsh39@DESKTOP-39V3VNQ>
2024-07-19 01:13:12 +0800

    2024/7/19

--- Lampyris Trade Station/Sources/UI/MainPartUI/PanelMainUITrade.cpp
+++ Lampyris Trade Station/Sources/UI/MainPartUI/PanelMainUITrade.cpp
@@ -4,12 +4,18 @@
 
 // Project Include(s)
 #include "PanelMainUITrade.h"
+#include <Base/Localization.h>
 
 PanelMainUITrade::PanelMainUITrade(QWidget* parent) : QWidget(parent) {
 	this->m_ui.setupUi(this);
 }
 
 void PanelMainUITrade::createPlaceOrderPartUI() {
+	QObject::connect(this->m_ui.tabTradeDirection, &QTabWidget::currentChanged, [=](int index) {
+		
+	});
+	this->m_ui.tabTradeDirection->setTabText(0, Localization->get("TradeDirectionBuy"));
+	this->m_ui.tabTradeDirection->setTabText(1, Localization->get("TradeDirectionSell"));
 }
 
 void PanelMainUITrade::createOrderPartUI() {
